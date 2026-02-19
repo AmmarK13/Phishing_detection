8ac50e5aaac4636a348ef5398c90f3f2281da947
Mulaimboy <bscs23059@itu.edu.pk>
2026-02-19 16:40:37 +0500

    new folder structure

---
+++ src/pipeline.py
@@ -0,0 +1,22 @@
+from feature_extraction import *
+import pandas as pd
+import argparse
+
+def main(input_path,output_path):
+    df=pd.read_csv(input_path)
+
+    #Extract features
+
+    df=add_features(df=df,url_column="url")
+
+    download_csv(df,output_path)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Add phishing URL features to a CSV dataset")
+    parser.add_argument("-i", "--input", required=True, help="Path to input CSV file")
+    parser.add_argument("-o", "--output", required=True, help="Path to save output CSV file")
+
+    args = parser.parse_args()
+    main(args.input, args.output)
